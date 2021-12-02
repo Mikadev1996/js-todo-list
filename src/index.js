aea1cd913545b2f3ca35f68b9210e9c688c17777
mika_laptop_windows <charmikadev@gmail.com>
2021-12-02 10:56:19 +0000

    todo list layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './style.css'
+
+function createElement(type, id, classes, text) {
+    const element = document.createElement(type);
+
+    if (id) {element.id = id}
+
+    if (classes) {
+        classes.forEach(classItem => {
+            element.classList.add(classItem);
+        })
+    }
+
+    if (text) {element.textContent = text}
+
+    return element;
+}
+
+export {createElement}
