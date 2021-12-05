dbd0931538e361697ce6714192ade08be907b2ac
Mikadev1996 <charmikadev@gmail.com>
2021-12-05 22:34:30 +0000

    checkbox button

--- src/mainPage.js
+++ src/mainPage.js
@@ -28,10 +28,23 @@ function refreshTodos() {
     todoUnorderedList.innerHTML = "";
 
     todos.forEach((todo) => {
-        let todoListItem = createElement("li", todo.id, ["todo", "todo-item"], todo.title);
+        let listItem = createElement("li", `list-${todo.project}-${todo.id}`, ["todo", "todo-item"]);
+
+        let listContentDiv = createElement("div", null, ["list-content"]);
+        let checkboxDiv = createElement("div", null, ["checkbox-div"]);
+        let checkBoxBtn = createElement("button", `checkbox-${todo.id}`, ["checkbox-btn"]);
+        let checkboxImage = createElement("img", null, ["checkbox"]);
+        checkboxImage.src = "https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2018/png/iconmonstr-check-mark-circle-thin.png&r=242&g=242&b=242";
+        checkBoxBtn.append(checkboxImage);
+        checkboxDiv.append(checkBoxBtn);
+
+        let todoTextDiv = createElement("div", null, ["list-text"], todo.title);
         let todoDescription = createElement("p", null, null, todo.description);
-        todoListItem.append(todoDescription);
-        todoUnorderedList.append(todoListItem);
+        todoTextDiv.append(todoDescription);
+
+        listContentDiv.append(checkboxDiv, todoTextDiv);
+        listItem.append(listContentDiv);
+        todoUnorderedList.append(listItem);
     })
 
     const addTodoBtnListItem = createElement("li", "new-todo-list-item", ["todo", "todo-btn"]);
@@ -109,22 +122,34 @@ function mainPage() {
                     const todayViewDiv = createElement("div", "today-view");
                     const listsBox = createElement("div", "lists-box");
                     const listsDiv = createElement("div", "lists");
-                    const listSection = createElement("section", "section-list");
+                    const listSection = createElement("div", "section-list");
 
                     const listCollection = createElement("ul", null, ["list-collection"]);
 
                     const defaultList = createElement("li", null, ["list"]);
-                    const defaultListHeader = createElement("header", null, ["todo-header", "todo"], "Todo List Group 1");
+                    const defaultListHeader = createElement("header", null, ["todo-header", "todo"], " Todo List Group 1");
                     const todoUnorderedList = createElement("ul", "default", ["todo", "todo-list"]);
 
 
                     todos.forEach((todo) => {
-                        let todoListItem = createElement("li", todo.id, ["todo", "todo-item"], todo.title);
-                        let todoDescription = createElement("p", null, null, todo.description);
-                        todoListItem.append(todoDescription);
-                        todoUnorderedList.append(todoListItem);
-                    })
+                        let listItem = createElement("li", `list-${todo.project}-${todo.id}`, ["todo", "todo-item"]);
 
+                        let listContentDiv = createElement("div", null, ["list-content"]);
+                        let checkboxDiv = createElement("div", null, ["checkbox-div"]);
+                        let checkBoxBtn = createElement("button", `checkbox-${todo.id}`, ["checkbox-btn"]);
+                        let checkboxImage = createElement("img", null, ["checkbox"]);
+                        checkboxImage.src = "https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../assets/preview/2018/png/iconmonstr-check-mark-circle-thin.png&r=242&g=242&b=242";
+                        checkBoxBtn.append(checkboxImage);
+                        checkboxDiv.append(checkBoxBtn);
+
+                        let todoTextDiv = createElement("div", null, ["list-text"], todo.title);
+                        let todoDescription = createElement("p", null, null, todo.description);
+                        todoTextDiv.append(todoDescription);
+
+                        listContentDiv.append(checkboxDiv, todoTextDiv);
+                        listItem.append(listContentDiv);
+                        todoUnorderedList.append(listItem);
+                    })
 
                     const addTodoBtnListItem = createElement("li", "new-todo-list-item", ["todo", "todo-btn"]);
                     const addTodoBtn = createElement("button", null, ["add-todo"], "+ Add Todo");
@@ -183,13 +208,14 @@ function addTodoUI() {
         function options() {
             const optionsDiv = createElement("div", null, ["options"]);
             const confirmDiv = createElement("div");
-            const confirmBtn = createElement("button", "default-confirm", ["option-btn", "confirm-add-todo"], "Add task");
+            const confirmBtn = createElement("button", "default-confirm", ["option-btn", "add-todo"], "Confirm Task");
             confirmDiv.append(confirmBtn)
 
             confirmBtn.addEventListener("click", () => {
 
                 confirmAddTodo("default");
             })
+
 
             const cancelDiv = createElement("div");
             const cancelBtn = createElement("button", null, ["option-btn", "cancel-add-todo"], "Cancel");
