9d00b00f46e840f270625166c61a2c9d7707f3f1
mika_laptop_windows <charmikadev@gmail.com>
2021-12-09 12:26:04 +0000

    overlay onclick listener

--- src/add-project-ui.js
+++ src/add-project-ui.js
@@ -4,15 +4,31 @@ function addProjectListener() {
     const addProjectBtn = document.getElementById("add-project");
     const editor = document.getElementById("editor");
     const overlay = document.getElementById("overlay");
+    console.log(addProjectBtn, editor, overlay)
+    // addProjectBtn.onclick = () => {
+    //     overlay.style.opacity = "1";
+    //     overlay.style.pointerEvents = "all";
+    // };
 
+    // overlay.onclick = () => {
+    //     overlay.style.opacity = "0";
+    //     overlay.style.pointerEvents = "none";
+    // }
 
-    addProjectBtn.onclick = () => {
-        overlay.style.opacity = "1";
-        overlay.style.pointerEvents = "all";
+    window.addEventListener("click", handleClick)
 
-    };
+    function handleClick(input) {
+        console.log(input.target.id);
+        if (input.target.id === "overlay") {
+            overlay.style.opacity = "0";
+            overlay.style.pointerEvents = "none";
+        }
+        else if (input.target.id === "add-project") {
+            overlay.style.opacity = "1";
+            overlay.style.pointerEvents = "all";
+        }
+    }
 }
-
 
 function overlay() {
     const overlay = createElement("div", "overlay");
